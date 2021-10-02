569846e2c435af5adfafb5ccfee558333cf676c9
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2021-10-02 14:15:36 -0400

    Multi Jump, Player Sticking to Platforms Fixed

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,77 +10,72 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundObjects;
     [SerializeField] private float checkRadius;
+    [SerializeField] private int maxJumpCount;
+
 
     private Rigidbody2D rb;
     private bool facingRight = true;
     private float moveDirection;
     private bool isJumping = false;
     private bool isGrounded = false;
+    private int jumpCount;
 
     // Awake is called before the Start, in the step phase in untiy
-    private void Awake()
-	{
+    private void Awake(){
         rb = GetComponent<Rigidbody2D>();
 	}
 
 	// Start is called before the first frame update
-	void Start()
-    {
-        
+	void Start(){
+        jumpCount = maxJumpCount;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         Inputs();
 
         Animations();
     }
 
 	//Physics and movement Update
-    void FixedUpdate()
-	{
+    void FixedUpdate(){
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
-
+        if(isGrounded){
+            jumpCount = maxJumpCount;
+        }
         Move();
     }
 
 
 	//user Inputs
-	private void Inputs()
-	{
+	private void Inputs(){
         moveDirection = Input.GetAxis("Horizontal");
-		if (Input.GetButtonDown("Jump"))
-		{
+		if (Input.GetButtonDown("Jump") && jumpCount > 0) {
             isJumping = true;
 		}
     }
 
     //animation
-    private void Animations()
-	{
+    private void Animations(){
         //character sprite face the direction they are moving
-        if ((moveDirection > 0 && !facingRight) || (moveDirection < 0 && facingRight))
-        {
+        if ((moveDirection > 0 && !facingRight) || (moveDirection < 0 && facingRight)) {
             FlipCharacter();
         }
     }
 
     //movement
-    private void Move()
-	{
+    private void Move(){
         rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
-		if (isJumping && isGrounded)
-		{
+		if (isJumping)
+        {
             rb.AddForce(new Vector2(0f, jumpForce));
+            jumpCount--;
             isJumping = false;
 		}
     }
 
-    private void FlipCharacter()
-	{
+    private void FlipCharacter(){
         facingRight = !facingRight;
         transform.Rotate(0, 180, 0);
-
 	}
 }
