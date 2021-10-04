8319dcf0159dead4441ada19e1f8a62e5fa8394c
loudram <sgusick@uvm.edu>
2021-10-04 18:04:18 -0400

    animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,65 +22,91 @@ public class PlayerMovement : MonoBehaviour
     private int jumpCount;
 
     public bool hasPowers;
-    // Awake is called before the Start, in the step phase in untiy
-    private void Awake(){
-        
-        rb = GetComponent<Rigidbody2D>();
-	}
 
-	// Start is called before the first frame update
-	void Start(){
+    public Animator animator;
+    // Awake is called before the Start, in the step phase in untiy
+    private void Awake()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
         playerScriptInstance = GetComponent<PlayerMovement>();
         jumpCount = maxJumpCount;
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
         Inputs();
 
         Animations();
     }
 
-	//Physics and movement Update
-    void FixedUpdate(){
+    //Physics and movement Update
+    void FixedUpdate()
+    {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
-        if(isGrounded){
+        if (isGrounded)
+        {
             jumpCount = maxJumpCount;
         }
         Move();
     }
 
 
-	//user Inputs
-	private void Inputs(){
+    //user Inputs
+    private void Inputs()
+    {
         moveDirection = Input.GetAxis("Horizontal");
-		if (Input.GetButtonDown("Jump") && jumpCount > 0) {
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
             isJumping = true;
-		}
+        }
     }
 
     //animation
-    private void Animations(){
+    private void Animations()
+    {
         //character sprite face the direction they are moving
-        if ((moveDirection > 0 && !facingRight) || (moveDirection < 0 && facingRight)) {
+        if ((moveDirection > 0 && !facingRight) || (moveDirection < 0 && facingRight))
+        {
             FlipCharacter();
         }
     }
 
     //movement
-    private void Move(){
+    private void Move()
+    {
         rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
-		if (isJumping)
+        if (isJumping)
         {
             rb.velocity = new Vector2(moveDirection * moveSpeed, 0);
             rb.AddForce(new Vector2(0f, jumpForce));
             jumpCount--;
             isJumping = false;
-		}
+            animator.SetBool("isRunning", false);
+        }
+
+        if (Mathf.Abs(rb.velocity.y) > 0.1f) {
+                animator.SetBool("isRunning", false);   
+        } else {
+            if (Mathf.Abs(rb.velocity.x) > 0.1f) {
+                animator.SetBool("isRunning", true);   
+            } else{
+                animator.SetBool("isRunning", false);
+            }
+        }
+
+
     }
 
-    private void FlipCharacter(){
+    private void FlipCharacter()
+    {
         facingRight = !facingRight;
         playerGFX.Rotate(0, 180, 0);
-	}
+    }
 }
