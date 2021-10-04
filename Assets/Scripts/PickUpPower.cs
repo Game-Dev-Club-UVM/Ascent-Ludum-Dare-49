91c8e1af83e5d3704f61628b403a55caf93c2c50
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2021-10-03 23:38:50 -0400

    Pickup

---
+++ Assets/Scripts/PickUpPower.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpPower : MonoBehaviour
+{
+	[SerializeField] GameObject enable;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		
+	}
+}
